60b8f03f5a902cf4d9f21e343bb2cdc8229d7c35
Varokas Panusuwan <varokas@panusuwan.com>
2014-07-15 17:18:05 -0700

    add b wins case

--- kata-swift-test/kata_tennis_test.swift
+++ kata-swift-test/kata_tennis_test.swift
@@ -4,13 +4,18 @@ class Tennis {
     let scoreWords = [0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"]
     
     var scoreA:Int = 0
+    var scoreB:Int = 0
     
     func aWins() {
         scoreA++
     }
     
+    func bWins() {
+        scoreB++
+    }
+    
     func getScore() -> String {
-        return "\(scoreWords[scoreA]) - Love"
+        return "\(scoreWords[scoreA]) - \(scoreWords[scoreB])"
     }
 }
 
@@ -36,9 +41,30 @@ class kata_tennis_test: XCTestCase {
         XCTAssertEqual(tennis.getScore(), "Forty - Love")
     }
     
+    func test_getScore_afterBWinsIs_FifteenLove() {
+        letBWins(1)
+        XCTAssertEqual(tennis.getScore(), "Love - Fifteen")
+    }
+    
+    func test_getScore_afterBWins2TimesIs_ThirtyLove() {
+        letBWins(2)
+        XCTAssertEqual(tennis.getScore(), "Love - Thirty")
+    }
+    
+    func test_getScore_afterBWins3TimesIs_FortyLove() {
+        letBWins(3)
+        XCTAssertEqual(tennis.getScore(), "Love - Forty")
+    }
+    
     func letAWins(times: Int) {
         for i in 1...times {
             tennis.aWins()
+        }
+    }
+    
+    func letBWins(times: Int) {
+        for i in 1...times {
+            tennis.bWins()
         }
     }
 
